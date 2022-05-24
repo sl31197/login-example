9efcd32febaa29f9faf399174072512e0237c245
devloperjy <devloperjy@Juui-MacBookPro.local>
2022-05-24 01:18:31 -0700

    js file is added.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const route_home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use('/', route_home);
 
 
